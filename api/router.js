9f017544569a6166dc2b094c5bdea7e40115c0d5
Kyle Lynds <kylelynds@Kyles-MBP.lan>
2020-12-17 13:56:33 -0800

    everything but the tests

--- api/router.js
+++ api/router.js
@@ -1,5 +1,39 @@
 const express = require("express");
+const db = require("../data/dbConfig");
+const bcrypt = require("bcryptjs");
 
 const router = express.Router();
+
+router.get("/users", (req, res) => {
+    db("users")
+      .then((data) => {
+        res.status(200).json(data);
+      })
+      .catch((err) => {
+        res.status(500).json({ message: err.message });
+      });
+  });
+
+  router.post("/register", (req, res) => {
+    console.log("registering");
+    const hash = bcrypt.hashSync(req.body.password, 10);
+  
+    const newUser = {
+      username: req.body.username,
+      password: hash,
+      department: req.body.department,
+    };
+  
+    db("users")
+      .insert(newUser)
+      .then((data) => {
+        res
+          .status(200)
+          .json({ message: `new user with an id of ${data} created` });
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.code });
+      });
+  });
 
 module.exports = router;
